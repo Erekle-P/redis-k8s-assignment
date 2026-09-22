d0dde4c60bc2605563192a4b5a8d20276e5fe9a4
Erekle-P <erekle.papuashvili@gmail.com>
2026-09-21 20:56:18 -0400

    Add EKS Terraform outputs

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -13,3 +13,23 @@ output "public_subnet_ids" {
 output "availability_zones" {
   value = module.vpc.azs
 }
+
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_version" {
+  value = module.eks.cluster_version
+}
+
+output "eks_oidc_provider_arn" {
+  value = module.eks.oidc_provider_arn
+}
+
+output "eks_managed_node_groups" {
+  value = module.eks.eks_managed_node_groups
+}
