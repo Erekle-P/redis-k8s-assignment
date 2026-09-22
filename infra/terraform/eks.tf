1af9e73b29f9effbffb6c3add310712ef401dcd0
Erekle-P <erekle.papuashvili@gmail.com>
2026-09-21 20:56:11 -0400

    Add EKS cluster and managed node group

---
+++ infra/terraform/eks.tf
@@ -0,0 +1,44 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "21.25.0"
+
+  name               = var.cluster_name
+  kubernetes_version = var.kubernetes_version
+
+  endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  addons = {
+    coredns = {}
+
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+
+    kube-proxy = {}
+
+    vpc-cni = {
+      before_compute = true
+    }
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+
+  tags = {
+    Name = var.cluster_name
+  }
+}
